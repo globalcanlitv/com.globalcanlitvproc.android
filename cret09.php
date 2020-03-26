1ac8454bbe2d8013fb880d82f6845f02db182c8b
globalcanlitv <53581316+globalcanlitv@users.noreply.github.com>
2020-03-26 17:33:13 +0100

    Ccc

--- cret09.php
+++ cret09.php
@@ -1,1 +1,8 @@
-{"deviceNo":"40571","deviceHash":"49b263bda07a9f37a9e8b9526b340ff1391f5cad","updateMessage":"Uygulamay\u0131 kullanabilmek i\u00e7in g\u00fcncelleme yapman\u0131z gerekmektedir.","updatePackageName":"","updateServer":"https://iptvpro93.github.io/download.html","updateStatus":false,"adsPeriod":"1","adsTimeout":"60","adMobBannerAds":true,"adMobInterstitialAds":true,"mediaServerUrl":"https://github.com/globalcanlitv/file/raw/master/tvlist4.txt?Id=3&DeviceNo=40571&Key=49b263bda07a9f37a9e8b9526b340ff1391f5cad","reportServerUrl":""}
+{"deviceNo":"40571","deviceHash":"49b263bda07a9f37a9e8b9526b340ff1391f5cad","updateMessage":"
+
+
+
+
+
+Uygulamay\u0131 kullanabilmek i\u00e7in g\u00fcncelleme yapman\u0131z gerekmektedir.
+NOT:Yeni sürümü indirdiğinizde önce eskisini kaldırın sonra yenisini kurun.","updatePackageName":"","updateServer":"https://iptvpro93.github.io/download.html","updateStatus":true,"adsPeriod":"1","adsTimeout":"60","adMobBannerAds":true,"adMobInterstitialAds":true,"mediaServerUrl":"https://github.com/globalcanlitv/file/raw/master/tvlist4.txt?Id=3&DeviceNo=40571&Key=49b263bda07a9f37a9e8b9526b340ff1391f5cad","reportServerUrl":""}
