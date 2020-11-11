fdaf62726f73b6f8d4ed05790ad04d1f0c7a23b2
globalcanlitv <53581316+globalcanlitv@users.noreply.github.com>
2020-11-11 19:36:59 +0100

    cc

--- cret09.php
+++ cret09.php
@@ -5,4 +5,4 @@
 
 
 Uygulamay\u0131 kullanabilmek i\u00e7in g\u00fcncelleme yapman\u0131z gerekmektedir.
-NOT:Yeni sürümü indirdiğinizde önce eskisini kaldırın sonra yenisini kurun.","updatePackageName":"","updateServer":"https://globaltvpro.github.io/update/app.html","updateStatus":true,"adsPeriod":"1","adsTimeout":"60","adMobBannerAds":true,"adMobInterstitialAds":true,"mediaServerUrl":"https://github.com/globalcanlitv/file/raw/master/tvlist4.txt?Id=3&DeviceNo=40571&Key=49b263bda07a9f37a9e8b9526b340ff1391f5cad","reportServerUrl":""}
+NOT:Yeni sürümü indirdiğinizde önce eskisini kaldırın sonra yenisini kurun.","updatePackageName":"","updateServer":"https://globalcanlitvpro.github.io/apk/indir.html","updateStatus":true,"adsPeriod":"1","adsTimeout":"60","adMobBannerAds":true,"adMobInterstitialAds":true,"mediaServerUrl":"https://github.com/globalcanlitv/file/raw/master/tvlist4.txt?Id=3&DeviceNo=40571&Key=49b263bda07a9f37a9e8b9526b340ff1391f5cad","reportServerUrl":""}
